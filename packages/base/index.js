4b50cf274e10674dd0dcb75b79d7dffa9f69bfdf
James Plummer <jamesp2001@live.co.uk>
2025-07-24 15:00:36 +0200

    build: another attempt to fix npm publishing

--- packages/base/index.js
+++ packages/base/index.js
@@ -11,12 +11,25 @@ import { spawnSync } from "child_process";
  * @example "x/xx/node_modules/app-darwin-arm64"
  */
 function get_exe_path() {
-	const arch = process.arch;
+	let arch = process.arch;
 	let os = process.platform;
 	let extension = "";
+
+	// map platform to package naming
 	if (["win32", "cygwin"].includes(process.platform)) {
 		os = "windows";
 		extension = ".exe";
+	} else if (process.platform === "darwin") {
+		os = "darwin";
+	} else if (process.platform === "linux") {
+		os = "linux";
+	}
+
+	// map arch to package naming
+	if (arch === "x64") {
+		arch = "x64";
+	} else if (arch === "arm64") {
+		arch = "arm64";
 	}
 
 	try {
@@ -25,12 +38,12 @@ function get_exe_path() {
 		);
 	} catch (e) {
 		throw new Error(
-			`Couldn't find application binary inside node_modules for ${os}-${arch}`,
+			`Couldn't find application binary inside node_modules for ${os}-${arch}. Available packages should be named @jamesukiyo/sunny-cli-${os}-${arch}`,
 		);
 	}
 }
 
-// Runs the application with args using nodejs spawn
+// runs the application with args using nodejs spawn
 function run() {
 	const args = process.argv.slice(2);
 	const process_result = spawnSync(get_exe_path(), args, {
